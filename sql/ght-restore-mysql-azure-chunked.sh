24202bc73b257335704bffc6eae1a9551c547592
Russell Pecka <rpecka98@gmail.com>
2018-03-22 16:39:27 -0400

    Added logic to split files and upload them in chunks

--- sql/ght-restore-mysql-azure-chunked.sh
+++ sql/ght-restore-mysql-azure-chunked.sh
@@ -5,7 +5,8 @@ user="ghtorrent"
 passwd=""
 host="localhost"
 db="ghtorrent"
-engine="MyISAM"
+engine="InnoDB"
+splitDir="split-dir"
 
 usage()
 {
@@ -16,8 +17,8 @@ usage()
   echo "    -p database passwd (default: $passwd)"
   echo "    -h database host (default: $host)"
   echo "    -d database to restore to. Must exist. (default: $db)"
-  echo "    -e db engine: MyISAM for fast import and querying speed"
-  echo "                  InnoDB for normal operations (default: $engine)"
+  echo "    -e db engine: InnoDB for normal operations (default: $engine)"
+  echo "                  MyISAM for fast import and querying speed"
 }
 
 if [ -z $1 ]
@@ -34,6 +35,7 @@ do
   h)  host=$OPTARG ;;
   d)  db=$OPTARG ;;
   e)  engine=$OPTARG ;;
+  s)  splitDir=$OPTARG
   \?)     echo "Invalid option: -$OPTARG" >&2
     usage
     exit 1
@@ -74,10 +76,33 @@ $mysql
 
 # 2. Restore CSV files with disabled FK checks
 for f in $dumpDir/*.csv ; do
-  table=`basename $f|cut -f1 -d'.'`
+  echo "`date` Removing split directory: $splitDir"
+  rm -rf "$splitDir"
+  echo "`date` Removed split directory"
+
+  echo "Creating new split directory"
+  mkdir "$splitDir"
+
+  echo "Copying $f to $splitDir"
+  cp "$f $splitDir"
+  echo "Done copying $f to splitDir"
+
+  echo "Splitting $f in $splitDir"
+  split -l 500000 -d "$f $f"
+  echo "Done splitting $f in $splitDir"
+
+  echo "Removing $f from $splitDir"
+  rm -rf "$splitDir/$f"
+  echo "Removed $f from $splitDir"
+
+  for s in $splitDir/*.csv* ; do
+  	table=`basename $f|cut -f1 -d'.'`
   echo "`date` Restoring table $table"
 
-  echo "SET foreign_key_checks = 0; LOAD DATA LOCAL INFILE '$f' INTO TABLE $table CHARACTER SET UTF8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' " |$mysql || exit 1
+  # Make sure to use LOAD DATA LOCAL in the following command because other loads are not permitted by azure
+  echo "SET foreign_key_checks = 0; LOAD DATA LOCAL INFILE '$s' INTO TABLE $table CHARACTER SET UTF8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' " |$mysql || exit 1
+  done
+
 done
 
 # 3. Create indexes
